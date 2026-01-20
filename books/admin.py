c1c52ab71007c1ed98651cce64173e62ac618102
voxid2030 <you@example.com>
2026-01-20 12:00:28 +0500

    users yaratildi

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Book, Author, BookAuthor, BookReview
 
-admin.site.register(Book)
 
-admin.site.register(Author)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'isbn')
+    list_filter = ('title',)
+    list_display = ('title', 'isbn', 'description')
+
+class AuthorAdmin(admin.ModelAdmin):
+        search_fields = ('first_name','last_name','email',)
+        list_filter = ('first_name','last_name','email',)
+        list_display = ('first_name','last_name','email',)
+
+
+admin.site.register(Book, BookAdmin)
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(BookAuthor)
 admin.site.register(BookReview)
 
