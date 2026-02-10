5503e59c7653539d00026695cc3821318fb8ad13
voxid2030 <you@example.com>
2026-02-10 17:33:09 +0500

    test

--- users/forms.py
+++ users/forms.py
@@ -30,3 +30,10 @@ class UserCreateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+
+    def save(self,commit=True):
+        user = super().save(commit)
+        user.set_password(self.cleaned_data['password'])
+        user.save()
+        return user        
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class RegistrationTestCase(TestCase):
+    def test_user_accout_is_created(self):
+        self.client.post(
+            #"/users/register",
+            reverse("users:register"),
+            data={
+                "username": 'Vohidjon',
+                "first_name": 'Vohidjon',
+                "last_name": 'Azimqulov',
+                "email": 'admin@mail.ru',
+                "password": '12345'
+            }
+        )
+        user = User.objects.get(username='Vohidjon')
+        self.assertEqual(user.username,'Vohidjon')
+        self.assertEqual(user.last_name,'Azimqulov')
+        self.assertEqual(user.email,'admin@mail.ru')
+        self.assertNotEqual(user.password, '12345')
+        self.assertTrue(user.check_password('12345'))
+
+    def test_required_fields(self):
+            response = self.client.post(
+                 reverse('users:register'),
+                 data= {
+                      "first_name": 'Vohidjon',
+                      "email": 'admin@mail.ru',
+                 }
+            )
+            user_count = User.objects.count()
+            self.assertEqual(user_count, 0)
+            self.assertFormError(response, 'form', 'username', 'This field is required.')
+            self.assertFormError(response, 'form', 'password', 'This field is required.')
+
+    def test_invalid_email(self):
+        response = self.client.post(
+            reverse('users:register'),
+            data={
+                "username": 'Vohidjon',
+                "first_name": 'Vohidjon',
+                "last_name": 'Azimqulov',
+                "email": 'invalid-email',
+                "password": '12345'
+            }
+        )
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 0)
+        self.assertFormError(response, 'form', 'email', 'Enter a valid email address.')
+               
